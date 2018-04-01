2e290be04374c48a3e0ef0d45c84c5c4398b558b
Alejandro C <35779075+AlejandroCruzB@users.noreply.github.com>
2018-04-01 17:05:47 +0200

    Add files via upload
    8.2 corregido

--- Lesson 8/8.2 FizzBuzz.py
+++ Lesson 8/8.2 FizzBuzz.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-print "FizzBuzz"
-fb = raw_input("Please enter a number between 1 and 100: ")
-try:
-    fb = int(fb)
-
-    for num in range(1, fb + 2):
-        if num % 3 == 0 and num % 5 == 0:
-            print "fizzbuzz"
-        elif num % 5 == 0:
-            print "buzz"
-        elif num % 3 == 0:
-            print "fizz"
-        else:
-            print num
-except Exception as e:
-    print "Please enter a number, not text!"
-
-
 while True:
+    print "FizzBuzz"
+    fb = raw_input("Please enter a number between 1 and 100: ")
+
+    try:
+        fb = int(fb)
+
+        for num in range(1, fb + 2):
+            if num % 3 == 0 and num % 5 == 0:
+                print "fizzbuzz"
+            elif num % 5 == 0:
+                print "buzz"
+            elif num % 3 == 0:
+                print "fizz"
+            else:
+                print num
+    except Exception as e:
+        print "Please enter a number, not text!"
+
+
+
     choice = raw_input("Would you like to do another number (y/n): ")
     if choice.lower() != "y" and choice.lower() != "yes":
         break
