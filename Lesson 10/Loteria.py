965d79ef75224a56b05cb300b3a1609872867121
Alejandro C <35779075+AlejandroCruzB@users.noreply.github.com>
2018-04-01 17:02:42 +0200

    Add files via upload
    lesson 10 terminada

--- Lesson 10/Loteria.py
+++ Lesson 10/Loteria.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from random import randint
-print 'Welcome to the Lottery numbers generator.'
-opcion = int(raw_input('Please enter how many random numbers would you like to have: '))
-opciones_restantes = opcion
-numero_aleatorio=[]
-while opciones_restantes > 0:
+import random
 
-    numero_aleatorio = randint(0, 50)
-    print numero_aleatorio,
-    opciones_restantes -= 1
+def main():
+    print 'Welcome to the Lottery numbers generator.'
+    opcion = int(raw_input('Please enter how many random numbers would you like to have: '))
+    try:
+        x = int(opcion)
+        print numero_aleatorio(x)
+    except ValueError:
+        print "Por favor inserte un numero"
 
-print 'Gracias por participar'
+    print "Gracias por participar"
+
+
+def numero_aleatorio(x):
+    numero_aleatorio_lista = []
+    while True:
+        if len(numero_aleatorio_lista) == x:
+            break
+
+        numero_aleatorio = randint(0, 9)
+
+        if numero_aleatorio not in numero_aleatorio_lista:
+            numero_aleatorio_lista.append(numero_aleatorio)
+
+    return numero_aleatorio_lista
+
+if __name__ == "__main__":
+    main()
